244bbd40203af495b52fe10f667012f822d2f1fe
harakaki <portgas0207@gmail.com>
2022-01-04 03:47:02 +0900

    2nd fin

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,9 +1,10 @@
 <x-layout>
     <x-slot name="title">
-        {{ $post }} - laravel test
+        {{ $post->title }} - laravel test
     </x-slot>
     <div class="back-link">
         &laquo; <a href="{{ route('posts.index') }}">Back</a>
     </div>
-    <h1>{{ $post }}</h1>
+    <h1>{{ $post->title }}</h1>
+    <p>{{ $post->body }}</p>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ use App\Http\Controllers\PostController;
 
 Route::get('/', [PostController::class, 'index'])->name('posts.index');
 
-Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
+Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
